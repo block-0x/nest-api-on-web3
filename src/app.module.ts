3968695e3691a42208b26031693d856c0fd48f6f
Iida-Jun <gum5588@gmail.com>
2022-01-30 22:48:31 +0900

    create

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NftModule } from './nft/nft.module';
 import { TransferModule } from './transfer/transfer.module';
+import { MintModule } from './mint/mint.module';
 
 @Module({
-  imports: [NftModule, TransferModule],
+  imports: [NftModule, TransferModule, MintModule],
   controllers: [AppController],
   providers: [AppService],
 })
